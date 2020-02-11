008dbf5c1fbbbffe2f0f55b21e3c6fd36d2778f6
Roman Atachiants <roman.atachiants@gmail.com>
2020-02-11 10:19:28 +0800

    relicense under MIT license

--- noop_test.go
+++ noop_test.go
@@ -1,18 +1,6 @@
 // +build !js
-
-/**********************************************************************************
-* Copyright (c) 2009-2018 Misakai Ltd.
-* This program is free software: you can redistribute it and/or modify it under the
-* terms of the GNU Affero General Public License as published by the  Free Software
-* Foundation, either version 3 of the License, or(at your option) any later version.
-*
-* This program is distributed  in the hope that it  will be useful, but WITHOUT ANY
-* WARRANTY;  without even  the implied warranty of MERCHANTABILITY or FITNESS FOR A
-* PARTICULAR PURPOSE.  See the GNU Affero General Public License  for  more details.
-*
-* You should have  received a copy  of the  GNU Affero General Public License along
-* with this program. If not, see<http://www.gnu.org/licenses/>.
-************************************************************************************/
+// Copyright (c) 2009-2020 Misakai Ltd. and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
 
 package stats
 
